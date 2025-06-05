ed97525718e239cdeeda3b2bc7d0ceee2560b622
Bilal Ahmad <bilalkhokhar228@gmail.com>
2025-06-05 22:30:01 +0500

    added ../ in index.js

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ const { router: paymentRouter, webhook } = require('./routes/paymentRoutes');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // Configure Stripe webhook route before other middleware
